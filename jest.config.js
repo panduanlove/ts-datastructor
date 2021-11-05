0602c957ab635b4f2708a847f993a2f9f33f3574
panfengshan <panfengshan@haodf.com>
2021-11-05 18:27:29 +0800

    first commit

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'ts'],
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.ts$': 'ts-jest' // ts 文件用 ts-jest 转换
+  },
+  // 匹配 __tests__ 目录下的 .js/.ts 文件 或其他目录下的 xx.test.js/ts xx.spec.js/ts
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts)$'
+};
---
+++ src/linklist/__test__/LinkList.test.ts
@@ -0,0 +1,5 @@
+import judge from '../LinkNode';
+
+test('test linkList Size to equal 0', () => {
+  expect(judge()).toEqual(3);
+});
